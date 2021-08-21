4cd46e30f842a9c870af84e9e6b89d349d61233a
nkoporec <me@nkoporec.com>
2021-08-21 10:38:24 +0200

    wip

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/gin-gonic/gin v1.7.4
 	github.com/ilyakaznacheev/cleanenv v1.2.5
+	github.com/jroimartin/gocui v0.5.0
 	github.com/skanehira/tson v0.0.0-20191202135716-4f590aae4ec8
 	github.com/spf13/cobra v1.2.1
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
--- internal/app/cmd/listen.go
+++ internal/app/cmd/listen.go
@@ -4,13 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nkoporec/dump/internal/http"
 	"github.com/spf13/cobra"
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
 var listenCmd = &cobra.Command{
 		Use:   "listen",
 		Short: "Starts a debugging server.",
 		Run: func(cmd *cobra.Command, args []string) {
-			listen()
+			go listen()
+			gui()
 		},
 }
 
@@ -23,4 +28,38 @@ func listen() {
 	handler := gin.New()
 	http.NewRouter(handler)
 	handler.Run()
+}
+
+
+func gui() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
