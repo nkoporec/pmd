89ed702851fd53394509a8c94ff3fc574572ba41
nkoporec <me@nkoporec.com>
2022-04-28 17:14:58 +0200

    fix clear command

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -161,7 +161,7 @@ func clearScreen(list *widgets.List, paragraph *widgets.Paragraph, breakpoint_po
 	// Clear list and paragraph
 	list.Rows = []string{}
 	paragraph.Text = ""
-	breakpoint_pos = 0
+	breakpoint_pos = 1
 	return list, paragraph, 0
 }
 
