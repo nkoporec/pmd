6723df81d36305d80c85761e8ee7492fbb79c6b8
nkoporec <me@nkoporec.com>
2022-05-13 17:02:43 +0200

    Make sure we only have once instance of pmd running at the same time

--- internal/app/cmd/listen.go
+++ internal/app/cmd/listen.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	nhttp "net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nkoporec/pmd/internal/http"
 	"github.com/nkoporec/pmd/internal/ui"
@@ -8,27 +12,45 @@ import (
 )
 
 var listenCmd = &cobra.Command{
-		Use:   "listen",
-		Short: "Starts a debugging server.",
-		Run: func(cmd *cobra.Command, args []string) {
-			messages := make(chan interface{})
+	Use:   "listen",
+	Short: "Starts a debugging server.",
+	Run: func(cmd *cobra.Command, args []string) {
+		messages := make(chan interface{})
 
-			go startServer(messages)
-			displayUi(messages)
-		},
+		go startServer(messages)
+		displayUi(messages)
+	},
 }
 
 func init() {
-  RootCmd.AddCommand(listenCmd)
+	RootCmd.AddCommand(listenCmd)
 }
 
 func startServer(messages chan interface{}) {
 	gin.SetMode(gin.ReleaseMode)
 
-	// Start http server.
-	handler := gin.New()
-	http.NewRouter(handler, messages, cch)
-	handler.Run(cfg.Yaml.Host + ":" + cfg.Yaml.Port)
+	// Check if port is free.
+	// For some reason gin is allowing
+	// multiple connections to the same port
+	// so we use a simple GET request to check if the
+	// host is free.
+	resp, err := nhttp.Get("http://" + cfg.Yaml.Host + ":" + cfg.Yaml.Port)
+	if err != nil {
+		// Start http server.
+		handler := gin.New()
+		http.NewRouter(handler, messages, cch)
+		handler.Run(cfg.Yaml.Host + ":" + cfg.Yaml.Port)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(
+		fmt.Sprintln(
+			"Host %s:%s is already in use.",
+			cfg.Yaml.Host,
+			cfg.Yaml.Port,
+		),
+	)
+	os.Exit(1)
 }
 
 func displayUi(messages chan interface{}) {
